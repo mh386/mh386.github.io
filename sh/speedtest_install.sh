e3187c9f34c52175ae522495606174b8e5f478bb
mh386 <47062144+mh386@users.noreply.github.com>
2022-08-22 03:27:20 -0700

    +: added sh (shell script) directory

---
+++ sh/speedtest_install.sh
@@ -0,0 +1,9 @@
+!/bin/sh
+
+cd ~
+mkdir speedtest
+cd speedtest
+wget https://install.speedtest.net/app/cli/ookla-speedtest-1.2.0-linux-x86_64.tgz
+tar zxvf ookla-speedtest-1.2.0-linux-x86_64.tgz
+./speedtest
+

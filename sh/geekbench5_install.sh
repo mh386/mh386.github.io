10cb55dc986f4488e60e5a79eaeb018ca4043c78
mh386 <47062144+mh386@users.noreply.github.com>
2022-08-22 22:13:30 -0700

    +: added geekbench5_install.sh script

---
+++ sh/geekbench5_install.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+cd ~
+mkdir geekbench5
+cd geekbench5
+wget 'https://cdn.geekbench.com/Geekbench-5.4.1-Linux.tar.gz'
+tar zxvf Geekbench-5.4.1-Linux.tar.gz
+cd Geekbench-5.4.1-Linux/
+clear
+./geekbench5
+
+
